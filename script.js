0c68b74909ac585c43e3e75f87631ba99e5060eb
Michael <104983553+Michael-Johansson@users.noreply.github.com>
2022-11-13 23:37:53 +0100

    Fixed grid scaling

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const container = document.querySelector(".container");
+const div = document.createElement("div");
+const child = document.querySelector(".child");
+
+const createGrid = (size) => {
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.classList.add("child");
+    container.append(div);
+
+    div.addEventListener("mouseover", () => {
+      div.classList.add("draw");
+    });
+  }
+
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+};
+
+createGrid(16);
