9eada0ef0823f4e974d64444e0dc04a39371156d
Michael <104983553+Michael-Johansson@users.noreply.github.com>
2022-11-15 02:40:24 +0100

    added grid control buttons

--- script.js
+++ script.js
@@ -3,7 +3,13 @@
 const container = document.querySelector(".container");
 const div = document.createElement("div");
 const child = document.querySelector(".child");
+const newGrid = document.getElementById("new-grid");
+const erase = document.getElementById("erase");
+const colorPick = document.getElementById("color-pick");
+const rainbow = document.getElementById("rainbow");
+const color = document.getElementById("color");
 
+//Creates a grid of divs with event listeners
 const createGrid = (size) => {
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
@@ -11,12 +17,33 @@ const createGrid = (size) => {
     container.append(div);
 
     div.addEventListener("mouseover", () => {
-      div.classList.add("draw");
+      // div.classList.add("draw");
+      div.style.backgroundColor = color.value;
     });
   }
-
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 };
+
+// Removes original grid completely and creates a new grid based on user input
+newGrid.addEventListener("click", (gridSize) => {
+  gridSize = prompt("Choose a grid size between 1 - 64");
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  if (gridSize > 64) {
+    alert("Grid size must be between 1 - 64");
+  } else {
+    createGrid(gridSize);
+  }
+});
+
+// Removes all colors from the grid
+erase.addEventListener("click", () => {
+  container.childNodes.forEach((node) => {
+    // node.classList.remove("draw");
+    node.style.backgroundColor = "white";
+  });
+});
 
 createGrid(16);
