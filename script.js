b9a763680fbbe0d8128134733b9974178bab31c1
Michael <104983553+Michael-Johansson@users.noreply.github.com>
2022-11-18 00:28:03 +0100

    added rainbow mode

--- script.js
+++ script.js
@@ -8,17 +8,25 @@ const erase = document.getElementById("erase");
 const colorPick = document.getElementById("color-pick");
 const rainbow = document.getElementById("rainbow");
 const color = document.getElementById("color");
+const rainbowR = () => Math.round(Math.random());
+const rainbowG = () => Math.round(Math.random());
+const rainbowB = () => Math.round(Math.random());
+let rainbowMode = false;
 
 //Creates a grid of divs with event listeners
 const createGrid = (size) => {
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
-    div.classList.add("child");
     container.append(div);
 
     div.addEventListener("mouseover", () => {
-      // div.classList.add("draw");
-      div.style.backgroundColor = color.value;
+      if (rainbowMode === true) {
+        div.style.backgroundColor = `rgb(${rainbowR() * 200},
+        ${rainbowG() * 200},
+        ${rainbowB() * 200}, 0.85)`;
+      } else {
+        div.style.backgroundColor = color.value;
+      }
     });
   }
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -33,6 +41,7 @@ newGrid.addEventListener("click", (gridSize) => {
   }
   if (gridSize > 64) {
     alert("Grid size must be between 1 - 64");
+    createGrid(16);
   } else {
     createGrid(gridSize);
   }
@@ -41,9 +50,17 @@ newGrid.addEventListener("click", (gridSize) => {
 // Removes all colors from the grid
 erase.addEventListener("click", () => {
   container.childNodes.forEach((node) => {
-    // node.classList.remove("draw");
     node.style.backgroundColor = "white";
   });
+});
+
+// Toggles rainbow mode
+rainbow.addEventListener("click", () => {
+  if (rainbowMode) {
+    rainbowMode = false;
+  } else if (!rainbowMode) {
+    rainbowMode = true;
+  }
 });
 
 createGrid(16);
