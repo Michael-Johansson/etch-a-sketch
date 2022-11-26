8c1fc2806ed8b4fe98776030a7ff9d5bbac8ce65
Michael <104983553+Michael-Johansson@users.noreply.github.com>
2022-11-26 22:21:07 +0100

    added text to show current grid size

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const container = document.querySelector(".container");
 const newGrid = document.getElementById("new-grid");
+const gridInfo = document.getElementById("grid-info");
 const erase = document.getElementById("erase");
 const colorPick = document.getElementById("color-pick");
 const rainbow = document.getElementById("rainbow");
@@ -13,10 +14,6 @@ const rainbowG = () => Math.round(Math.random());
 const rainbowB = () => Math.round(Math.random());
 let rainbowMode = false;
 let mouseDown = false;
-colorWrapper.addEventListener("change", () => {
-  colorWrapper.style.backgroundColor = color.value;
-})
-
 
 //Creates a grid of divs with event listeners
 const createGrid = (size) => {
@@ -25,6 +22,7 @@ const createGrid = (size) => {
     container.append(div);
     colorWrapper.style.backgroundColor = color.value;
 
+    // This makes it possible to color a square with a click, instead of only mouseover
     div.addEventListener("mousedown", () => {
       if (rainbowMode === true) {
         div.style.backgroundColor = `rgb(${rainbowR() * 200},
@@ -33,7 +31,7 @@ const createGrid = (size) => {
       } else {
         div.style.backgroundColor = color.value;
       }
-    })
+    });
 
     div.addEventListener("mouseover", () => {
       if (mouseDown) {
@@ -45,15 +43,16 @@ const createGrid = (size) => {
           div.style.backgroundColor = color.value;
         }
       }
-      
     });
   }
+
+  // Toggles "mouseDown" so the user is drawing only while holding down the mouse button
   window.addEventListener("mousedown", () => {
     mouseDown = true;
-  })
+  });
   window.addEventListener("mouseup", () => {
     mouseDown = false;
-  })
+  });
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 };
@@ -66,9 +65,11 @@ newGrid.addEventListener("click", (gridSize) => {
   }
   if (gridSize > 64) {
     alert("Grid size must be between 1 - 64");
+    gridInfo.textContent = "16 x 16";
     createGrid(16);
   } else {
     createGrid(gridSize);
+    gridInfo.textContent = `${gridSize} x ${gridSize}`;
   }
 });
 
@@ -84,14 +85,19 @@ rainbow.addEventListener("click", () => {
     rainbow.style.background = "white";
   } else if (!rainbowMode) {
     rainbowMode = true;
-    rainbow.style.background = "linear-gradient(0deg, #ff4800, #ffee00, #00ff2a, #00ccff, #0011ff)";
+    rainbow.style.background =
+      "linear-gradient(0deg, #ff4800, #ffee00, #00ff2a, #00ccff, #0011ff)";
   }
 });
 
 outline.addEventListener("click", () => {
   container.childNodes.forEach((node) => {
-    node.classList.toggle("outline")
-  })
-})
+    node.classList.toggle("outline");
+  });
+});
+
+colorWrapper.addEventListener("change", () => {
+  colorWrapper.style.backgroundColor = color.value;
+});
 
 createGrid(16);
