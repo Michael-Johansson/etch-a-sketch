189bedf57ef128484009f8cc9eabff81a9a4ddef
Michael <michael_405@outlook.com>
2023-05-15 01:19:41 +0200

    remaking everything

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 "use strict";
 
-const container = document.getElementById("container");
-const gridSizeBtn = document.getElementById("grid-size-btn");
-const overlay = document.querySelector(".overlay");
+const container = document.querySelector("container");
 
-function createGrid(gridSize) {
-  for (let i = 0; i < gridSize * gridSize; i++) {
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
     const div = document.createElement("div");
-    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    container.append(div);
+    
   }
 }
-
-gridSizeBtn.addEventListener("click", () => {
-  overlay.classList.toggle("active");
-})
-
-createGrid(5);
